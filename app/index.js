201b71b351e1f0ebefeeb8b45a3f0dad53949755
Gabriel Vautrin <g.vautrin@bee-buzziness.com>
2021-01-25 16:45:40 +0100

    refactor(app): use async/await

--- app/index.js
+++ app/index.js
@@ -7,48 +7,37 @@ navigator.serviceWorker.ready
     .then(function (registration) {
         return registration.pushManager.getSubscription()
             .then(async function (subscription) {
-                if (subscription) {
-                    return subscription;
-                }
-
                 // Get the server's public key
                 const response = await fetch(`${baseUrl}/vapidPublicKey`);
                 const vapidPublicKey = await response.text();
 
                 const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
 
-                return registration.pushManager.subscribe({
-                    userVisibleOnly: true,
-                    applicationServerKey: convertedVapidKey
-                });
+                if (!subscription) {
+                    subscription = await registration.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: convertedVapidKey
+                    })
+
+                    await fetch(`${baseUrl}/register`, {
+                        method: 'post',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            subscription: subscription
+                        }),
+                    });
+                }
+
+                document.getElementById('doIt').onclick = function () {
+                    fetch(`${baseUrl}/send`, {
+                        method: 'post',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: {},
+                    });
+                };
             });
-    }).then(function (subscription) {
-
-    fetch(`${baseUrl}/register`, {
-        method: 'post',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            subscription: subscription
-        }),
-    });
-
-    document.getElementById('doIt').onclick = function () {
-        const delay = document.getElementById('notification-delay').value;
-        const ttl = document.getElementById('notification-ttl').value;
-
-        fetch(`${baseUrl}/send`, {
-            method: 'post',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                subscription: subscription,
-                delay: delay,
-                ttl: ttl,
-            }),
-        });
-    };
-
 });
