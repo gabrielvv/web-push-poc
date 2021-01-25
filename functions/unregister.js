affab19bb70d55a7d8fe3dbcbeaaac0faaf8ee9c
Gabriel Vautrin <g.vautrin@bee-buzziness.com>
2021-01-25 13:57:00 +0100

    feat: front app

---
+++ functions/unregister.js
@@ -0,0 +1,26 @@
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+exports.handler = async (event, context) => {
+    /* configure faunaDB Client with our secret */
+    const client = new faunadb.Client({
+        secret: process.env.FAUNADB_SERVER_SECRET
+    })
+    const data = JSON.parse(event.body)
+    console.log(`Function 'unregister' invoked. delete id: ${data.endpoint}`)
+    return client.query(q.Delete(q.Ref(`${process.env.FAUNADB_COLLECTION}/${id}`)))
+        .then((response) => {
+            console.log('success', response)
+            return {
+                statusCode: 200,
+                body: JSON.stringify(response)
+            }
+        }).catch((error) => {
+            console.log('error', error)
+            return {
+                statusCode: 400,
+                body: JSON.stringify(error)
+            }
+        })
+}
