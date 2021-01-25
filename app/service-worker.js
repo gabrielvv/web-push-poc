c2ac6e4cf31b1cc3f025fbbcbb75ec3ab5f68217
Gabriel Vautrin <g.vautrin@bee-buzziness.com>
2021-01-25 15:00:45 +0100

    feat: show push notif in service worker

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,10 +1,9 @@
-// Register event listener for the 'push' event.
-self.addEventListener('push', function(event) {
-    // Keep the service worker alive until the notification is created.
+self.addEventListener('push', function (event) {
+    const payload = event.data ? event.data.text() : 'no payload';
     event.waitUntil(
-        // Show a notification with title 'ServiceWorker Cookbook' and body 'Alea iacta est'.
         self.registration.showNotification('ServiceWorker Cookbook', {
-            body: 'Alea iacta est',
+            body: payload,
         })
     );
 });
+
